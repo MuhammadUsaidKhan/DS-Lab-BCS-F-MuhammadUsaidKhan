79e1a96944990fb0bcd4bd4620cc8dc2394473e6
Muhammad Usaid Khan <muhammedusaid.khan@gmail.com>
2025-08-29 13:22:16 +0500

    Update Task03.cpp

--- Lab 02/Task03.cpp
+++ Lab 02/Task03.cpp
@@ -1,52 +1,109 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Course{
+
+class Course {
     string courseName;
     float marks;
-    public:
-    Course(){
+
+public:
+    Course() {
         courseName = "";
-        marks =0;
+        marks = 0;
     }
-    Course(string cn, float m){
-        courseName = cn;
-        marks= m;
+
+    void inputCourse() {
+        cout << "Enter course name: ";
+        cin >> courseName;
+        cout << "Enter marks: ";
+        cin >> marks;
+    }
+
+    float getMarks() {
+        return marks;
+    }
+
+    string getCourseName() {
+        return courseName;
     }
 };
-class Student{
+
+class Student {
     string name;
     string rollno;
     string university;
     int numCourses;
     Course* courses;
-    public:
-    Student(){
-        name="";
-        rollno="";
-        university="";
-        courses = 0;
+
+public:
+    Student() {
+        name = "";
+        rollno = "";
+        university = "";
+        numCourses = 0;
+        courses = nullptr;
     }
-    Student(string n, string rn, string u, int c){
-        name = n;
-        rollno = rn;
-        university = u;
+
+    void inputStudent() {
+        cout << "Enter name: ";
+        cin >> name;
+        cout << "Enter roll number: ";
+        cin >> rollno;
+        cout << "Enter university: ";
+        cin >> university;
+        cout << "Enter number of courses: ";
+        cin >> numCourses;
+
         courses = new Course[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            cout << "Course " << i + 1 << ":\n";
+            courses[i].inputCourse();
+        }
+    }
+
+    float calculateAverage() {
+        float sum = 0;
+        for (int i = 0; i < numCourses; i++) {
+            sum += courses[i].getMarks();
+        }
+        return (numCourses > 0) ? (sum / numCourses) : 0;
+    }
+
+    void displayStudent() {
+        cout << "\nStudent Name: " << name << endl;
+        cout << "Roll Number: " << rollno << endl;
+        cout << "University: " << university << endl;
+        cout << "Courses and Marks:\n";
+        for (int i = 0; i < numCourses; i++) {
+            cout << "- " << courses[i].getCourseName() << ": " << courses[i].getMarks() << endl;
+        }
+        cout << "Average Marks: " << calculateAverage() << endl;
+    }
+
+    ~Student() {
+        delete[] courses;
     }
 };
+
 int main() {
     int students;
-    cout<<"Enter Number Of Students: "<<endl;
-    cin>>students;
-    Student *stu;
-    stu = new Student[students];
-    int size[students];
-    for(int i=0; i<students; i++){
-        cout<<"Enter Number of Courses of Student: "<<endl;
-        cin>>size[i];
-        stu = new Student[size[i]];
+    cout << "Enter number of students: ";
+    cin >> students;
+
+    Student* stu = new Student[students];
+
+    for (int i = 0; i < students; i++) {
+        cout << "\nEnter details for Student " << i + 1 << ":\n";
+        stu[i].inputStudent();
     }
-    
-    
+
+    cout << "\n----- Student Report -----\n";
+    for (int i = 0; i < students; i++) {
+        stu[i].displayStudent();
+    }
+
+    delete[] stu;
     return 0;
 }
+
